2bc0c6a1e9f6d0c01113c40d2f9e9268b9141d07
CharmanT <charmant@tut.by>
2018-04-17 18:25:26 +0300

    drag and drop items in user dialog

--- js/dialog.js
+++ js/dialog.js
@@ -1,37 +1,84 @@
-var dialogHandle = userDialog.querySelector('.setup-user-pic');
+'use strict';
 
-dialogHandle.addEventListener('mousedown', function (evt) {
-  evt.preventDefault();
+(function () {
+  var userDialog = document.querySelector('.setup');
+  var dialogHandle = userDialog.querySelector('.setup-user-pic');
 
-  var startCoords = {
-    x: evt.clientX,
-    y: evt.clientY
-  };
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
 
-  var onMouseMove = function (moveEvent) {
-    moveEvent.preventDefault();
-
-    var shift = {
-      x: startCoords.x - moveEvent.clientX,
-      y: startCoords.y - moveEvent.clientY
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
     };
 
-    startCoords = {
-      x: moveEvent.clientX,
-      y: moveEvent.clientY
+    var onMouseMove = function (moveEvent) {
+      moveEvent.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvent.clientX,
+        y: startCoords.y - moveEvent.clientY
+      };
+
+      startCoords = {
+        x: moveEvent.clientX,
+        y: moveEvent.clientY
+      };
+
+      userDialog.style.top = (userDialog.offsetTop - shift.y) + 'px';
+      userDialog.style.left = (userDialog.offsetLeft - shift.x) + 'px';
     };
 
-    userDialog.style.top = (userDialog.offsetTop - shift.y) + 'px';
-    userDialog.style.left = (userDialog.offsetLeft - shift.x) + 'px';
-  };
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
 
-  var onMouseUp = function (upEvt) {
-    upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
 
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', onMouseUp);
-  };
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
 
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
-});
+  // drag and drop items in user dialog
+  var shopElement = document.querySelector('.setup-artifacts-shop');
+  var artifactsElement = document.querySelector('.setup-artifacts');
+
+  var draggedItem = null;
+
+  shopElement.addEventListener('dragstart', function (evt) {
+
+    if (evt.target.tagName.toLowerCase() === 'img') {
+    draggedItem = evt.target;
+    evt.dataTransfer.setData('text/plan', evt.target.alt);
+    artifactsElement.style.outline = '2px dashed red';
+    };
+  });
+
+  shopElement.addEventListener('dragend', function (evt) {
+      artifactsElement.removeAttribute('style');
+  });
+
+  artifactsElement.addEventListener('dragover', function (evt) {
+    evt.preventDefault();
+    return false;
+  });
+
+  artifactsElement.addEventListener('drop', function (evt) {
+    evt.target.removeAttribute('style');
+    evt.target.appendChild(draggedItem.cloneNode(true));
+    evt.preventDefault();
+  });
+
+
+  artifactsElement.addEventListener('dragenter', function (evt) {
+    evt.target.style.backgroundColor = 'yellow';
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragleave', function (evt) {
+    evt.target.style.backgroundColor = '';
+
+    evt.preventDefault();
+  });
+})();
