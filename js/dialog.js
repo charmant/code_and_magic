f3dced7a498bfb77226ea4f26f6423f9a000d125
CharmanT <charmant@tut.by>
2018-04-19 13:15:53 +0300

    moved 'open and close popup' function

--- js/dialog.js
+++ js/dialog.js
@@ -1,9 +1,53 @@
 'use strict';
 
 (function () {
+  var ESC_KEYCODE = 27;
+  var ENTER_KEYCODE = 13;
   var userDialog = document.querySelector('.setup');
   var dialogHandle = userDialog.querySelector('.setup-user-pic');
+  var setupOpen = document.querySelector('.setup-open');
+  var setupUserName = userDialog.querySelector('.setup-user-name');
+  var setupClose = userDialog.querySelector('.setup-close');
 
+  // open and close user dialog
+  var onPopupEscPress = function (evt) {
+    if (evt.keyCode === ESC_KEYCODE && !(setupUserName === document.activeElement)) {
+      closePopup();
+    }
+  };
+
+  var openPopup = function () {
+    userDialog.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+  };
+
+  var closePopup = function () {
+    userDialog.classList.add('hidden');
+    document.removeEventListener('keydown', onPopupEscPress);
+    userDialog.removeAttribute('style');
+  };
+
+  setupOpen.addEventListener('click', function () {
+    openPopup();
+  });
+
+  setupOpen.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      openPopup();
+    }
+  });
+
+  setupClose.addEventListener('click', function () {
+    closePopup();
+  });
+
+  setupClose.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      closePopup();
+    }
+  });
+
+  //drag and move user dialog
   dialogHandle.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
 
